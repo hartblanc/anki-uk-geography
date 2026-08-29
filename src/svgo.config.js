7d0ce255f734762c185bdba967d2c1a0d8047b74
callum <notify+github@callumhart.me>
2026-08-29 22:08:43 +0100

    perf: disable pretty SVG output in SVGO
    Removes indentation from the minified layer SVGs, shaving ~1.2 KB off
    maps.js (whitespace-only, no visual change).

--- src/svgo.config.js
+++ src/svgo.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   multipass: true,
   floatPrecision: 1,
-  js2svg: {
-    indent: 2, // string with spaces or number of spaces. 4 by default
-    pretty: true, // boolean, false by default
-  },
   plugins: [
     {
       name: "preset-default",
