454074a342bc37264babd9eb75b12ea19020b452
callum <notify+github@callumhart.me>
2026-08-29 15:46:23 +0100

    chore(aggregate_csv): improve assertion failure message for missing cities

--- utils/uk_geog/aggregate_csvs.py
+++ utils/uk_geog/aggregate_csvs.py
@@ -31,10 +31,13 @@ fieldnames = [
     "tags",
 ]
 
+
 def key(d):
-    return tuple([d.get(k, '') for k in fieldnames])
+    return tuple([d.get(k, "") for k in fieldnames])
+
 
 A = TypeVar("A")
+
 
 def build_deck_csv(
     uk_regions_path: Path,
@@ -45,7 +48,6 @@ def build_deck_csv(
     outfile_path: Path,
     guids: dict,
 ) -> None:
-
     rows = []
     # Regions
     with uk_regions_path.open(mode="r") as csvfile:
@@ -67,11 +69,13 @@ def build_deck_csv(
     with uk_counties_path.open(mode="r") as csvfile:
         county_regions = list(csv.reader(csvfile))[1:]  # Skip header row
 
-    for (county_name, region_name) in county_regions:
+    for county_name, region_name in county_regions:
         try:
             county_country[county_name] = region_country[region_name]
         except KeyError:
-            raise Exception(f"Key error, could not find region {region_name} in region_country for county {county_name}")
+            raise Exception(
+                f"Key error, could not find region {region_name} in region_country for county {county_name}"
+            )
 
         rows.append(
             {
@@ -87,17 +91,15 @@ def build_deck_csv(
         body_of_water_names = list(csv.reader(csvfile))[1:]  # Skip header row
 
     for (bow_name,) in body_of_water_names:
-        rows.append(
-            {"location": bow_name, "bow": bow_name, "tags": "BoW"}
-        )
+        rows.append({"location": bow_name, "bow": bow_name, "tags": "BoW"})
 
     # Cities
     with city_county_csv_path.open(mode="r") as csvfile:
         city_counties = list(csv.reader(csvfile))[1:]  # Skip header row
 
     counties_by_city = dict()
-    for (city_name, county_name) in city_counties:
-        counties_by_city[city_name] = [ c.strip() for c in county_name.split("/")]
+    for city_name, county_name in city_counties:
+        counties_by_city[city_name] = [c.strip() for c in county_name.split("/")]
 
     with uk_cities_path.open(mode="r") as csvfile:
         city_names = list(csv.reader(csvfile))[1:]  # Skip header row
@@ -113,7 +115,9 @@ def build_deck_csv(
                 "tags": f"City, {country}",
             }
         )
-    assert set([c for (c,) in city_names]) == set(counties_by_city.keys())
+    assert set([c for (c,) in city_names]) == set(
+        counties_by_city.keys()
+    ), f"city counties csv mappsing - cities in map: {set(counties_by_city.keys()) - set([c for (c,) in city_names])}, cities in map - city counties csv mapping: {set(counties_by_city.keys()) - set([c for (c,) in city_names])}"
 
     for row in rows:
         try:
@@ -126,13 +130,12 @@ def build_deck_csv(
     rows.sort(key=lambda r: r.get("guid", ""))
 
     with outfile_path.open(mode="w") as outfile:
-        writer = csv.DictWriter(outfile, fieldnames=["guid"]+fieldnames)
+        writer = csv.DictWriter(outfile, fieldnames=["guid"] + fieldnames)
         writer.writeheader()
         writer.writerows(rows)
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(
         description="A utility for extracting data from SVG files into the brain brew CSV format",
     )
@@ -184,10 +187,7 @@ if __name__ == "__main__":
     with args.guids.open(mode="r") as curr_datafile:
         reader = csv.DictReader(curr_datafile)
         rows = list(reader)
-        curr_guids = {
-            key(row): row['guid']
-            for row in rows
-        }
+        curr_guids = {key(row): row["guid"] for row in rows}
 
     build_deck_csv(
         args.uk_regions,
@@ -196,5 +196,5 @@ if __name__ == "__main__":
         args.bodies_of_water,
         args.city_county_csv,
         args.outfile,
-        curr_guids
+        curr_guids,
     )
